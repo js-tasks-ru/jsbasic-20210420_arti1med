4af11af7e45bbef037b39531f2b162a42c3da767
Artem Medvedev <medvedev@dialweb.ru>
2021-04-25 18:29:41 +0300

    4 ЗАДАНИЕ ИСПРАВЛЕНО

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -2,10 +2,7 @@ function checkSpam(str) {
   let strNormal = str.replace(/\s+/g, '').toUpperCase();
   let strCheckSpamFirst = strNormal.includes('XXX'.toUpperCase());
   let strCheckSpamSecond = strNormal.includes('1xBet'.toUpperCase());
-  if(strCheckSpamFirst || strCheckSpamSecond){
-    return true;
-  }else{
-    return false;
 
-  }
+  return strCheckSpamFirst || strCheckSpamSecond;
+
 }
